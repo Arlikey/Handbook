4ac6e104aee2883b3d76ebfc4eb4a550375833bb
Arlikey <arkeyfast@gmail.com>
2023-10-26 18:40:30 +0300

    Add project files.

---
+++ Handbook.cpp
@@ -0,0 +1,70 @@
+#include "Handbook.h"
+
+Handbook::Handbook(string company_name, string owner, string tel_number, string adress, string occupation) {
+	this->company_name = company_name;
+	this->owner = owner;
+	this->tel_number = tel_number;
+	this->adress = adress;
+	this->occupation = occupation;
+}
+
+void Handbook::SetCompanyName(string company_name){
+	this->company_name = company_name;
+}
+
+void Handbook::SetOwner(string owner) {
+	this->owner = owner;
+}
+
+void Handbook::SetTelephoneNumber(string tel_number) {
+	this->tel_number = tel_number;
+}
+
+void Handbook::SetAdress(string adress) {
+	this->adress = adress;
+}
+
+void Handbook::SetOccupation(string occupation) {
+	this->occupation = occupation;
+}
+
+string Handbook::GetCompanyName() {
+	return company_name;
+}
+
+string Handbook::GetOwner() {
+	return owner;
+}
+
+string Handbook::GetTelephoneNumber() {
+	return tel_number;
+}
+
+string Handbook::GetAdress() {
+	return adress;
+}
+
+string Handbook::GetOccupation() {
+	return occupation;
+}
+
+void Handbook::Input() {
+	cout << "Enter company name: ";
+	cin >> company_name;
+	cout << "Enter company owner: ";
+	cin >> owner;
+	cout << "Enter telephone number: ";
+	cin >> tel_number;
+	cout << "Enter adress: ";
+	cin >> adress;
+	cout << "Enter occupation: ";
+	cin >> occupation;
+}
+
+void Handbook::Print() {
+	cout << "Company name: " << company_name << endl;
+	cout << "Owner: " << owner << endl;
+	cout << "Telephone number: " << tel_number << endl;
+	cout << "Adress: " << adress << endl;
+	cout << "Ocuppation: " << occupation << endl;
+}
---
+++ Handbook.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Handbook
+{
+	string company_name;
+	string owner;
+	string tel_number;
+	string adress;
+	string occupation;
+public:
+	Handbook() = default;
+	Handbook(string company_name, string owner, string tel_number, string adress, string occupation);
+	
+	void SetCompanyName(string company_name);
+	void SetOwner(string owner);
+	void SetTelephoneNumber(string tel_number);
+	void SetAdress(string adress);
+	void SetOccupation( string occupation);
+	
+	string GetCompanyName();
+	string GetOwner();
+	string GetTelephoneNumber();
+	string GetAdress();
+	string GetOccupation();
+
+	void Input();
+	void Print();
+};
+
